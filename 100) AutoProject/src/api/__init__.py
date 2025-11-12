b1172e466426668813e87ace13b12c460007a5f2
Skrppapa <kostymoskovskiy@mail.ru>
2025-11-12 21:18:39 +0300

    Все уроки + отделил свой проект

--- 100) AutoProject/src/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from src.api.cars import router as car_router
-from src.api.mainpage import router as mainpage_router
-# from src.api.users import router as user_router   - это когда юзеры появятся
-
-
-main_router = APIRouter()
-main_router.include_router(car_router)  # Делаем в этой папке главный роутер в котором будут стянуты все остальные и уже его импортируем в main.py
-main_router.include_router(mainpage_router)
--- 100) AutoProject/src/api/mainpage.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get('/', tags = ["Home"], summary = "Главная страница")
-def root():
-    return "Main Page"
